aa7d36fc867df01d4929dec741e76a420df7b750
sonmez27 <snmezmustafa@hotmail.com>
2021-12-12 10:36:33 +0300

    getData

---
+++ app.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const getData = async (number) => {
+
+    const {data : user } = await axios(`https://jsonplaceholder.typicode.com/users/${number}`)
+    const {data : post } = await axios(`https://jsonplaceholder.typicode.com/posts?id=${number}`)
+    return {"userInformation": user, "userPosts": post}
+}
+
+
+export default getData
